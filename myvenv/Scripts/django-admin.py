28dea6a70e2fa2e726e6619343b1087f13e300c5
Faith Clarisse Sotalbo <faithsotalbo@yahoo.com>
2018-12-05 21:33:17 +0800

    Initial commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\francisco\documents\dlsu\18-19 1st term\thes2\tutorials\trial\django\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
